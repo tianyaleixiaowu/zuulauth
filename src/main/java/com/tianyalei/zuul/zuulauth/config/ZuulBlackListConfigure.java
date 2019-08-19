f6fb7f87b370591fc6e181062eafe2387557c861
wuwf <272551766@qq.com>
2019-08-19 16:12:31 +0800

    加入zuul的ip黑白名单功能

--- src/main/java/com/tianyalei/zuul/zuulauth/config/ZuulBlackListConfigure.java
+++ src/main/java/com/tianyalei/zuul/zuulauth/config/ZuulBlackListConfigure.java
@@ -1,8 +1,10 @@
 package com.tianyalei.zuul.zuulauth.config;
 
+import com.tianyalei.zuul.zuulauth.zuul.filter.BlackListFilter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cloud.netflix.zuul.ZuulProxyMarkerConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -12,5 +14,8 @@ import org.springframework.context.annotation.Configuration;
 @ConditionalOnMissingBean(ZuulBlackListConfigure.class)
 @ConditionalOnBean(ZuulProxyMarkerConfiguration.class)
 public class ZuulBlackListConfigure {
-
+    @Bean
+    public BlackListFilter blackListFilter() {
+        return new BlackListFilter();
+    }
 }
--- src/main/java/com/tianyalei/zuul/zuulauth/config/ZuulWhiteListConfigure.java
+++ src/main/java/com/tianyalei/zuul/zuulauth/config/ZuulWhiteListConfigure.java
@@ -1,8 +1,10 @@
 package com.tianyalei.zuul.zuulauth.config;
 
+import com.tianyalei.zuul.zuulauth.zuul.filter.WhileListFilter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cloud.netflix.zuul.ZuulProxyMarkerConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -13,5 +15,9 @@ import org.springframework.context.annotation.Configuration;
 @ConditionalOnBean(ZuulProxyMarkerConfiguration.class)
 public class ZuulWhiteListConfigure {
 
+    @Bean
+    public WhileListFilter whileListFilter() {
+        return new WhileListFilter();
+    }
     
 }
